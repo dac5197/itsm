317df0bb579d97d6fec562397c0f2d7ce1f87d24
Don Chaney <dac5197@live.com>
2020-12-01 09:05:06 -0500

    Remove all files

--- base/utils.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-
-def get_random_alphanumeric_string(length=32):
-    letters_and_digits = string.ascii_letters + string.digits
-    result_str = ''.join((random.choice(letters_and_digits) for i in range(length)))
-    return result_str
--- tracking/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-
-from .models import *
-
-class WorkNoteForm(forms.ModelForm):
-    class Meta:
-        model = WorkNote
-        fields = '__all__'
