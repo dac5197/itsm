409696f96ce12751878a2f0fff759a0bd5a6d471
Don Chaney <dac5197@live.com>
2020-12-01 08:59:38 -0500

    Added gitignore and decouple settings

--- itsm/settings.py
+++ itsm/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import psycopg2
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%uj=r#$(8+8xc0*so@_4#-w96)=g#31)fbd+!8$6-j@#fqc!av'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -89,10 +90,10 @@ WSGI_APPLICATION = 'itsm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'itsm_dev',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST' : 'localhost'
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST' : config('DB_HOST')
     }
 }
 
